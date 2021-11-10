00a4d767a3d7ac52a0759ce715aca7e17455fc3c
k000 <kintore@mineo.jp>
2021-11-10 23:33:59 +0900

    暫定対応

--- app/Domain/Logic/Validation/EntryExitCreateValidation.php
+++ app/Domain/Logic/Validation/EntryExitCreateValidation.php
@@ -3,6 +3,7 @@
 namespace App\Domain\Logic\Validation;
 
 use App\Domain\Logic\Rule\EntryExit\TransactionCombinationRule;
+use App\Domain\Logic\Rule\Rule;
 use App\Domain\Model\Entity\EntryExitSlip;
 
 use function PHPUnit\Framework\isNull;
@@ -12,8 +13,12 @@ class EntryExitCreateValidation
 
     private array $rules = array();
 
+    private EntryExitSlip $slip;
+
     public function __construct(EntryExitSlip $slip)
     {
+        $this->slip = $slip;
+
         array_push($this->rules,[
             new TransactionCombinationRule($slip),
         ]);
@@ -21,9 +26,16 @@ class EntryExitCreateValidation
 
     public function execute()
     {
+
+        // TODO これはまずいので実装方法があり次第修正する
+        $rule = new TransactionCombinationRule($this->slip);
+        $rule->vaild();
+
         foreach($this->rules as $rule)
         {
-            $rule->vaild();
+
+            // Javaのようにできないので一旦、保留の処理になっています。
+            // $rule->vaild();
         }
     }
 

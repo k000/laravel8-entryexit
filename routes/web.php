96ff1b6a497f59c4a077cd75b5befea4e2c77456
k000 <kintore@mineo.jp>
2021-11-14 00:36:45 +0900

    ルーティング周りのミドルウェア設定

--- routes/web.php
+++ routes/web.php
@@ -20,33 +20,33 @@ Route::get('/', function () {
 
 // 一旦ガードなど抜きで考えて作成する
 // TODO groupとガードの設定をする
-Route::get('/entryexit/create', [App\Http\Controllers\EntryExitController::class, 'create'])->name('entryexitcreate');
-Route::post('/entryexit/store',[App\Http\Controllers\EntryExitController::class, 'store']);
+Route::get('/entryexit/create', [App\Http\Controllers\EntryExitController::class, 'create'])->middleware(['auth'])->name('entryexitcreate');
+Route::post('/entryexit/store',[App\Http\Controllers\EntryExitController::class, 'store'])->middleware(['auth']);
 
-Route::get('/entryexit/all',[App\Http\Controllers\EntryExitController::class, 'all']);
-Route::get('/entryexit/edit/{id}', [App\Http\Controllers\EntryExitController::class, 'edit'])->name('entryexitedit');
+Route::get('/entryexit/all',[App\Http\Controllers\EntryExitController::class, 'all'])->middleware(['auth']);
+Route::get('/entryexit/edit/{id}', [App\Http\Controllers\EntryExitController::class, 'edit'])->name('entryexitedit')->middleware(['auth']);
 
-Route::post('/entryexit/update',[App\Http\Controllers\EntryExitController::class, 'update']);
+Route::post('/entryexit/update',[App\Http\Controllers\EntryExitController::class, 'update'])->middleware(['auth']);
 
-Route::post('/entryexit/delete/{id}', [App\Http\Controllers\EntryExitController::class, 'delete'])->name('entryexitdelete');
+Route::post('/entryexit/delete/{id}', [App\Http\Controllers\EntryExitController::class, 'delete'])->middleware(['auth'])->name('entryexitdelete');
 
-Route::get('/stocks',[App\Http\Controllers\StockController::class, 'all']);
+Route::get('/stocks',[App\Http\Controllers\StockController::class, 'all'])->middleware(['auth']);
 
 
 
 
 Route::get('/item/create',function(){
     return view('item.create');
-});
+})->middleware(['auth:admin']);
 
-Route::post('/item/store',[App\Http\Controllers\ItemController::class, 'store']);
+Route::post('/item/store',[App\Http\Controllers\ItemController::class, 'store'])->middleware(['auth:admin']);
 
 
 Route::get('/warehouse/create',function(){
     return view('warehouse.create');
-});
+})->middleware(['auth:admin']);
 
-Route::post('/warehouse/store',[App\Http\Controllers\WarehouseController::class, 'store']);
+Route::post('/warehouse/store',[App\Http\Controllers\WarehouseController::class, 'store'])->middleware(['auth:admin']);
 
 
 
